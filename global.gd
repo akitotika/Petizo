1bb3e3a9d18b6fb28ef0d460ba41e87e2b5a5281
akitootika <akitotest01@gmail.com>
2024-04-03 11:14:07 -0300

    up

--- global.gd
+++ global.gd
@@ -37,14 +37,28 @@ func _ready():
 	for i in spliter.size():
 		print(spliter[i])
 		print("ready 1,2,3...")
+	save_me()
 
-
-func load():
-	pass
+func load_me():
+	var file = FileAccess.open("user://save_data.json",FileAccess.READ)
+	var json = file.get_as_text()
+	var save_data = JSON.parse_string(json)
+	print("...................................................................")
+	var info = save_data["data"]
+	var simple = info[1]
+	print(simple[0])
+	file.close()
+	
 	
 func save_me():
-	JSON 
-	pass
+	var file = FileAccess.open("user://save_data.json",FileAccess.WRITE)
+	var saved_data = {}
+	saved_data["data"]= grid
+	var json = JSON.stringify(saved_data)
+	file.store_string(json)
+	file.close()
+	load_me()
+	
 	
 
 
