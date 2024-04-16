69e08ebcd1cacad006148a3d42faed318b92cf4b
akitootika <akitotest01@gmail.com>
2024-04-16 11:13:09 -0400

    simple

--- Code/registro.gd
+++ Code/registro.gd
@@ -8,7 +8,7 @@ func _ready():
 	escribe()
 		
 func escribe():
-	if busca == "":
+	#if busca == "":
 		Global.load_me()
 		for n in Global.db_largo:
 			Global.D1 = str(Global.info[n][0]) + str(Global.info[n][1])
@@ -22,24 +22,23 @@ func escribe():
 			$Panel/ScrollContainer/VBoxContainer.add_child(dat)
 			index = str(Global.D1) + str(Global.D2) + str(Global.D3)
 			print(index)
-	else:
-		print(busca)
-		Global.load_me()
-		for n in Global.db_largo:
-			Global.D1 = str(Global.info[n][0]) + str(Global.info[n][1])
-			Global.D2 = str(Global.info[n][2]) + str(Global.info[n][3])
-			Global.D3 = str(Global.info[n][4]) + str(Global.info[n][5])
-			Global.fecha=str(Global.info[n][6])
-			Global.monto = str(Global.info[n][7])
-			Global.time_stamp = str(Global.info[n][8])
-			Global.id = Global.info[n][9]
-			if Global.D1 == busca:
-				$Panel/ScrollContainer/VBoxContainer.queue_free()
-				var dat =P.instantiate()
-				
-				$Panel/ScrollContainer/VBoxContainer.add_child(dat)
-				index = str(Global.D1) + str(Global.D2) + str(Global.D3)
-			print(index)
+	#else:
+	#	print(busca)
+	#	Global.load_me()
+	
+	#	for n in Global.db_largo:
+	#		Global.D1 = str(Global.info[n][0]) + str(Global.info[n][1])
+	#		Global.D2 = str(Global.info[n][2]) + str(Global.info[n][3])
+	#		Global.D3 = str(Global.info[n][4]) + str(Global.info[n][5])
+	#		Global.fecha=str(Global.info[n][6])
+	#		Global.monto = str(Global.info[n][7])
+	#		Global.time_stamp = str(Global.info[n][8])
+	#		Global.id = Global.info[n][9]
+	#		if Global.D1 == busca:
+	#			var dat =P.instantiate()
+	#			$Panel/ScrollContainer/VBoxContainer.add_child(dat)
+	#			index = str(Global.D1) + str(Global.D2) + str(Global.D3)
+	#		print(index)
 		
 		
 
@@ -54,5 +53,13 @@ func _on_button_3_button_up():
 
 
 func _on_busca_text_changed():
-	busca = %busca.text
-	escribe()
+	#busca = %busca.text
+	#escribe()
+	pass
+
+#var one = 0
+func _on_busca_focus_entered():
+	#if one == 0:
+	#	$Panel/ScrollContainer/VBoxContainer.queue_free()
+		#one = one + 1
+	pass
